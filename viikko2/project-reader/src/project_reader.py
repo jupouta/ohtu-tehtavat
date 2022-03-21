bde7687ac1fec09e5b9fef08430f05f112b9c7ce
Julia <juliapoutanen@gmail.com>
2022-03-21 14:35:13 +0200

    exercise 2: add toml

---
+++ viikko2/project-reader/src/project_reader.py
@@ -0,0 +1,20 @@
+import toml
+
+from urllib import request
+from project import Project
+
+
+class ProjectReader:
+    def __init__(self, url):
+        self._url = url
+
+    def get_project(self):
+        # tiedoston merkkijonomuotoinen sisältö
+        content = request.urlopen(self._url).read().decode("utf-8")
+        parsed_toml = toml.loads(content)['tool']['poetry']
+
+        # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
+        return Project(parsed_toml['name'],
+                       parsed_toml['description'],
+                       parsed_toml['dependencies'],
+                       parsed_toml['dev-dependencies'])
