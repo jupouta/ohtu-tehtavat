3e4d58c7898e0cbba3160115b0aec22a455e587a
Julia <juliapoutanen@gmail.com>
2022-04-05 13:39:50 +0300

    add exercise 3

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -1,5 +1,4 @@
-import requests
-from player import Player
+from player import PlayerReader, PlayerStats
 
 # {'name': 'Antti Suomela',
 # 'nationality': 'FIN',
@@ -9,35 +8,13 @@ from player import Player
 # 'team': 'SJS',
 # 'games': 4}
 
-def sorted_points(player):
-    result = player.goals + player.assists
-    return result
-
-
 def main():
     url = "https://nhlstatisticsforohtu.herokuapp.com/players"
-    response = requests.get(url).json()
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
 
-    print("JSON-muotoinen vastaus:")
-    #print(response)
-
-    players = []
-
-    for player_dict in response:
-        player = Player(
-            player_dict['name'],
-            player_dict['team'],
-            player_dict['goals'],
-            player_dict['assists']
-        )
-
-        players.append(player)
-
-    print("Oliot:")
-
-    sorted_players = sorted(players, reverse=True, key=sorted_points)
-
-    for player in sorted_players:
+    for player in players:
         print(player)
 
 if __name__ == "__main__":
--- viikko3/nhl-reader/src/player.py
+++ viikko3/nhl-reader/src/player.py
@@ -1,11 +1,56 @@
+import requests
+
+def sorted_points(player):
+    result = player.goals + player.assists
+    return result
+
 class Player:
-    def __init__(self, name, team, goals, assists):
+    def __init__(self, name, team, goals, assists, nationality):
         self.name = name
         self.team = team
         self.goals = goals
         self.assists = assists
+        self.nationality = nationality
 
     def __str__(self):
         combined = self.goals + self.assists
         stringi = f"{self.name:20} {self.team} {self.goals:2} + {self.assists:2} = {combined:2}"
         return stringi
+
+class PlayerReader:
+
+    def __init__(self, url):
+        self.url = url
+        self.players = self.get_players()
+
+    def get_players(self):
+        response = requests.get(self.url).json()
+
+        players = []
+
+        for player_dict in response:
+            player = Player(
+                player_dict['name'],
+                player_dict['team'],
+                player_dict['goals'],
+                player_dict['assists'],
+                player_dict['nationality']
+            )
+
+            players.append(player)
+        return players
+
+class PlayerStats:
+
+    def __init__(self, reader):
+        self.reader = reader
+
+    def top_scorers_by_nationality(self, nationality):
+        sorted_players = sorted(self.reader.players, reverse=True, key=sorted_points)
+
+        result = []
+
+        for player in sorted_players:
+            if player.nationality == nationality:
+                result.append(player)
+        return result
